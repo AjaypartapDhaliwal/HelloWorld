4ee549a561116e6f2f22544a0311bb131df22c64
AjaypartapD <LORDnugget12@hotmail.co.uk>
2023-09-08 12:25:27 +0100

    third commit

---
+++ src/variablesExercise/stringExercise.java
@@ -0,0 +1,23 @@
+package variablesExercise;
+
+public class stringExercise {
+
+    public static void main(String[] args) {
+
+        /*
+        String firstName = "first";
+        String lastName = "last";
+        String name = firstName + lastName;
+        System.out.println(name);
+         */
+
+        int num1 = 3;
+        double num2 = 5.0;
+        System.out.println(num1 + num2);
+        double result = num1 / num2;
+        int num3 = 8;
+        System.out.println(num3++);
+        System.out.println(num3);
+        System.out.println(++num3);
+    }
+}
